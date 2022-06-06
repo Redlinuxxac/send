5723c904f812cd54e8a1f1232a78591c2008119e
Edwin Rafael Rosario De La Rosa <rosarioedwinac@gmail.com>
2022-06-06 12:04:49 -0500

    Configurado para envios de multiple destinatarios.

--- src/Send.php
+++ src/Send.php
@@ -24,12 +24,29 @@ class Send
     static function toHtml($toAddress, $subject='Prueba', $mensaje='Prueba', $fromMail="inf@elizabethgconstruction.com")
     {
         // Para enviar un correo HTML, debe establecerse la cabecera Content-type
-        $para        = implode(",", $toAddress);
+        $para        = explode(",", $toAddress);
+        $nD=sizeof($para);
+        if($nD==1){
+            $para=$para[0];
+        }
+        else
+        {$n=1;$x='';
+            foreach($para as $pa)
+            {
+                if($n==$nD){
+                             $x.= $pa;
+                            }
+                else{
+                    $x.= $pa.',';
+                }
+                $n++;
+            }$para =$x;
+        }
         $cabeceras[] = 'MIME-Version: 1.0';
         $cabeceras[] = 'Content-type: text/html; charset=utf-8';   
         $cabeceras[] = 'To: '.$para;
         $cabeceras[] = 'From: '.$fromMail; 
-        $mensaje = view("$mensaje", compact('titulo', 'mensaje'));
+        $mensaje = view("$mensaje", compact('subject', 'mensaje'));
 
         mail($para, $subject, $mensaje, implode("\r\n", $cabeceras));
     }
