c8bb1e8032661860e0137294b67dce9df5b29cf2
Edwin Rafael Rosario De La Rosa <rosarioedwinac@gmail.com>
2022-06-06 13:27:42 -0500

    listo

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,9 @@ use Reddatasrd\Send\Send;
 use Reddatasrd\Send\SendFacade;
 
 Route::get('/send', function(){
-    $para='Rosarioedwinac@gmail.com';
+    $para='Rosarioedwinac@gmail.com,kmr105@hotmail.com';
     $titulo='Prueba Final';
-    $mensaje='Send::index.index';//Send::
-    return Send::toHtml($para, $titulo, $mensaje);
+    $vista='Send::index.index';//Send::
+    $mensaje='Envio de Prueba de nueva libreria crada por Edwin Rosario';//Send::
+    return Send::toHtml($para, $titulo, $mensaje,$vista);
 });
